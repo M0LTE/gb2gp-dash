6a4d2ded988f0978226349873143e6aef880e504
Tom <tj@tjej.uk>
2018-10-17 23:37:52 +0100

    Initial commit

--- gp2gp-dash/Program.cs
+++ gp2gp-dash/Program.cs
@@ -20,6 +20,7 @@ namespace gp2gp_dash
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseIISIntegration()
                 .Build();
     }
 }
--- gp2gp-dash/Startup.cs
+++ gp2gp-dash/Startup.cs
@@ -20,7 +20,7 @@ namespace gp2gp_dash
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
