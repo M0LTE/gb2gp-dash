b9a6346342fda4958154bc781bc18a67f1203e7f
Tom Fanning <tom@tomfanning.name>
2019-07-23 23:07:32 +0100

    Replace n1mm2web.csproj

---
+++ n1mm2web/Models/RadioState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gp2gp_dash.Models
+{
+    public class RadioState
+    {
+        public RadioState()
+        {
+            LastUpdated = DateTime.Now;
+        }
+
+        public DateTime LastUpdated { get; set; }
+        public string ID { get; set; }
+        public double Frequency { get; set; }
+    }
+}
